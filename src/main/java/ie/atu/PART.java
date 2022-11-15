2ea3223862bdbba80e5e3a883efc8124bd45c64a
Parthibg00410769 <g00410769@atu.ie>
2022-11-15 13:24:01 +0000

    FIRST TRY

--- src/main/java/ie/atu/PART.java
+++ src/main/java/ie/atu/PART.java
@@ -1,7 +1,16 @@
 package ie.atu;
-
+import java.io.*;
 
 
 public class PART {
+    public static void main(String[] args) {
+        {
+            File myfile = new File("Myfile.txt");
+            System.out.println("My file is located at "+ myfile.getAbsolutePath());
+
+
+        }
+
+    }
 
 }
