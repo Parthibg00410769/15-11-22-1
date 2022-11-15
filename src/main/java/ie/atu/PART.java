34366a9018c98f43b6dc920066a15ad39cf8c159
Parthibg00410769 <g00410769@atu.ie>
2022-11-15 14:13:23 +0000

    filewritertrycatch

--- src/main/java/ie/atu/PART.java
+++ src/main/java/ie/atu/PART.java
@@ -5,9 +5,18 @@ import java.io.*;
 public class PART {
     public static void main(String[] args) {
         {
-            File myfile = new File("Myfile.txt");
-            System.out.println("My file is located at "+ myfile.getAbsolutePath());
-
+            File myFile = new File("myFile.txt");
+            System.out.println("My file is located at "+ myFile.getAbsolutePath());
+            String content = "Details to write";
+            try
+            {
+                FileWriter myWriter = new FileWriter(myFile,false);
+                myWriter.write("This is group mmmmm");
+                myWriter.close();
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+            }
 
         }
 
